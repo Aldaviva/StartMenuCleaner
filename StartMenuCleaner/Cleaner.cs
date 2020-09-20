ab60de43d130fb0ec14937c2e300eab0010ca6cb
Ben Hutchison <ben@aldaviva.com>
2020-09-20 04:14:15 -0700

    Extracted file list to separate class. Allow glob patterns.

---
+++ StartMenuCleaner/Cleaner.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using static System.Environment.SpecialFolder;
+
+namespace StartMenuCleaner {
+
+    public static class Cleaner {
+
+        private static readonly string MACHINE_START_DIRECTORY = getStartMenuDirectory(false);
+        private static readonly string USER_START_DIRECTORY    = getStartMenuDirectory(true);
+
+        private static void Main() {
+            cleanStartMenu(DeletionPathRepository.defaultInstance);
+        }
+
+        public static void cleanStartMenu(DeletionPathRepository deletionPathRepository) {
+            foreach (string pathToDelete in from startMenuFileOrDirectory in findFilesAndDirectories(MACHINE_START_DIRECTORY).Concat(findFilesAndDirectories(USER_START_DIRECTORY))
+                                            where deletionPathRepository.patternsToDelete.Any(glob => glob.IsMatch(startMenuFileOrDirectory.relativePath))
+                                            select startMenuFileOrDirectory.absolutePath) {
+                deleteDirectoryOrFile(pathToDelete);
+            }
+        }
+
+        private static string getStartMenuDirectory(bool isUser) => Path.Combine(Environment.GetFolderPath(isUser ? ApplicationData : CommonApplicationData), @"Microsoft\Windows\Start Menu");
+
+        private static IEnumerable<FoundStartMenuPath> findFilesAndDirectories(string baseDirectory) =>
+            Directory.EnumerateFileSystemEntries(baseDirectory, "*", SearchOption.AllDirectories).Select(path => new FoundStartMenuPath(path, baseDirectory));
+
+        private static void deleteDirectoryOrFile(string pathToDelete) {
+            try {
+                Directory.Delete(pathToDelete, true);
+            } catch (DirectoryNotFoundException) {
+                // return successfully
+            } catch (IOException) {
+                // path may actually be a file, not a directory
+                File.Delete(pathToDelete);
+            }
+        }
+
+        private readonly struct FoundStartMenuPath {
+
+            internal readonly string relativePath;
+            internal readonly string absolutePath;
+
+            internal FoundStartMenuPath(string absolutePath, string baseDirectory) {
+                this.absolutePath = absolutePath;
+                relativePath = Path.GetRelativePath(baseDirectory, absolutePath);
+            }
+
+        }
+
+    }
+
+}
---
+++ StartMenuCleaner/DeletionPathRepository.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DotNet.Globbing;
+
+namespace StartMenuCleaner {
+
+    public interface DeletionPathRepository {
+
+        IEnumerable<Glob> patternsToDelete { get; }
+
+        static DeletionPathRepository defaultInstance => new DeletionPathRepositoryImpl();
+
+    }
+
+    internal class DeletionPathRepositoryImpl: DeletionPathRepository {
+
+        public IEnumerable<Glob> patternsToDelete { get; } = new List<string> {
+            @"Programs\Adobe After Effects *.lnk",
+            @"Programs\Adobe Audition *.lnk",
+            @"Programs\Adobe Media Encoder *.lnk",
+            @"Programs\Adobe Premiere Pro *.lnk",
+            @"Programs\JetBrains",
+            @"Programs\Logi",
+        }.Select(Glob.Parse).ToList();
+
+    }
+
+}
