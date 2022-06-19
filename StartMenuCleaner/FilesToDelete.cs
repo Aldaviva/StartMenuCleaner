e0d9cc8a160f3583db748bc76405197c24bc67ca
Ben Hutchison <ben@aldaviva.com>
2022-06-19 02:41:54 -0700

    Added 7+ Taskbar Tweaker and EVGA Precision X1

--- StartMenuCleaner/FilesToDelete.cs
+++ StartMenuCleaner/FilesToDelete.cs
@@ -1,51 +1,49 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using DotNet.Globbing;
+﻿using DotNet.Globbing;
 
-namespace StartMenuCleaner {
+namespace StartMenuCleaner;
 
-    internal static class FilesToDelete {
+internal static class FilesToDelete {
 
-        public static IEnumerable<Glob> patterns { get; } = new List<string> {
-            @"Programs\Accessories",
-            @"Programs\Accessibility",
-            @"Programs\Administrative Tools",
-            @"Programs\Adobe Acrobat *.lnk",
-            @"Programs\Adobe After Effects *.lnk",
-            @"Programs\Adobe Audition *.lnk",
-            @"Programs\Adobe Bridge *.lnk",
-            @"Programs\Adobe Creative Cloud.lnk",
-            @"Programs\Adobe Dreamweaver *.lnk",
-            @"Programs\Adobe Illustrator *.lnk",
-            @"Programs\Adobe InDesign *.lnk",
-            @"Programs\Adobe Lightroom Classic.lnk",
-            @"Programs\Adobe Media Encoder *.lnk",
-            @"Programs\Adobe Photoshop *.lnk",
-            @"Programs\Adobe Premiere Pro *.lnk",
-            @"Programs\Avidemux *",
-            @"Programs\Canon Utilities",
-            @"Programs\Fiddler *.lnk",
-            @"Programs\Git",
-            @"Programs\Git Extensions.lnk",
-            @"Programs\Hex Workshop v*",
-            @"Programs\JetBrains",
-            @"Programs\KeePass 2.lnk",
-            @"Programs\Launchy",
-            @"Programs\Logi",
-            @"Programs\Maxon",
-            @"Programs\Microsoft Edge.lnk",
-            @"Programs\Microsoft Office Tools",
-            @"Programs\Outlook.lnk",
-            @"Programs\Progress",
-            @"Programs\System Tools",
-            @"Programs\Steam",
-            @"Programs\TagScanner",
-            @"Programs\VideoLAN",
-            @"Programs\Visual Studio *",
-            @"Programs\Winaero Tweaker",
-            @"Programs\WinSCP.lnk",
-        }.Select(Glob.Parse).ToList();
-
-    }
+    public static IEnumerable<Glob> patterns { get; } = new List<string> {
+        @"Programs\7+ Taskbar Tweaker.lnk",
+        @"Programs\Accessibility",
+        @"Programs\Accessories",
+        @"Programs\Administrative Tools",
+        @"Programs\Adobe Acrobat *.lnk",
+        @"Programs\Adobe After Effects *.lnk",
+        @"Programs\Adobe Audition *.lnk",
+        @"Programs\Adobe Bridge *.lnk",
+        @"Programs\Adobe Creative Cloud.lnk",
+        @"Programs\Adobe Dreamweaver *.lnk",
+        @"Programs\Adobe Illustrator *.lnk",
+        @"Programs\Adobe InDesign *.lnk",
+        @"Programs\Adobe Lightroom Classic.lnk",
+        @"Programs\Adobe Media Encoder *.lnk",
+        @"Programs\Adobe Photoshop *.lnk",
+        @"Programs\Adobe Premiere Pro *.lnk",
+        @"Programs\Avidemux *",
+        @"Programs\Canon Utilities",
+        @"Programs\EVGA",
+        @"Programs\Fiddler *.lnk",
+        @"Programs\Git Extensions.lnk",
+        @"Programs\Git",
+        @"Programs\Hex Workshop v*",
+        @"Programs\JetBrains",
+        @"Programs\KeePass 2.lnk",
+        @"Programs\Launchy",
+        @"Programs\Logi",
+        @"Programs\Maxon",
+        @"Programs\Microsoft Edge.lnk",
+        @"Programs\Microsoft Office Tools",
+        @"Programs\Outlook.lnk",
+        @"Programs\Progress",
+        @"Programs\Steam",
+        @"Programs\System Tools",
+        @"Programs\TagScanner",
+        @"Programs\VideoLAN",
+        @"Programs\Visual Studio *",
+        @"Programs\Winaero Tweaker",
+        @"Programs\WinSCP.lnk",
+    }.Select(Glob.Parse).ToList();
 
 }
